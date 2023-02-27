d07d509d1f2dbd44fc4b504573ab609f21ee8470
Wasosky <110636486+0xWasosky@users.noreply.github.com>
2023-02-27 20:43:54 +0100

    Update libs.py

--- utils/libs.py
+++ utils/libs.py
@@ -5,5 +5,5 @@ install = "pip install "
 delete = "pip uninstall "
 update = "pip install --upgrade"
 
-load_libs = subprocess.getoutput("pip freeze")
+load_libs = subprocess.getoutput("pip freeze").splitlines()
 
